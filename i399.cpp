289bef35e755299107cf173968e67b04bba62449
jane8366608 <jane8366608@gmail.com>
2023-04-19 09:05:53 +0800

    Update i399.cpp

--- i399.cpp
+++ i399.cpp
@@ -1,2 +1,33 @@
-// i399. 數字遊戲 -- 2022年6月APCS(第1題)
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+// i399. 1. 數字遊戲 2022年6月APCS(第一題)
 // https://zerojudge.tw/ShowProblem?problemid=i399
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i=0, tmp=0, Arr[10]={0};
+    for(i=0;i<3;i++){
+        cin>>tmp;
+        Arr[tmp]++;
+    }
+
+    int max_cnt=0;
+    for(i=0;i<10;i++){
+        if(max_cnt<Arr[i])
+            max_cnt=Arr[i];
+    }
+    cout<<max_cnt<<" ";
+    for(i=9;i>0;i--){
+        if(Arr[i])
+            cout<<i<<" ";
+    }
+    return 0;
+}
