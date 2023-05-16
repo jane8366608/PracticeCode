0f8b7f7508fbfbc029706cebe3fb8cf4c4e06696
jane8366608 <jane8366608@gmail.com>
2023-05-16 14:46:13 +0800

    Create d074.py

---
+++ d074.py
@@ -0,0 +1,10 @@
+"""
+* ZeroJudge d074. 電腦教室
+* https://zerojudge.tw/ShowProblem?problemid=d074
+* Authored by Yang Yu-Chieh at 2023-05-16 14:42.
+* 。
+"""
+input()
+a = list(map(int, input().split()))
+a.sort()
+print(a[-1])
