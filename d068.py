4c9d4ac6d3f69427b18831db7aa159bae338ebc4
jane8366608 <jane8366608@gmail.com>
2023-05-16 09:57:17 +0800

    Create d068.py

---
+++ d068.py
@@ -0,0 +1,8 @@
+"""
+* ZeroJudge d068. 該減肥了！
+* https://zerojudge.tw/ShowProblem?problemid=d068
+* Authored by Yang Yu-Chieh at 2023-05-16 09:254.
+* 若大於50，則-1。
+"""
+w = int(input())
+print(w if w<=50 else w-1)
