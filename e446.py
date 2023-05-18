b4f2fd5367629c494a16a109739d4529366d7a4b
jane8366608 <jane8366608@gmail.com>
2023-05-18 17:16:42 +0800

    Update e446.py

--- e446.py
+++ e446.py
@@ -2,9 +2,7 @@
 * ZeroJudge e446. 排列生成
 * https://zerojudge.tw/ShowProblem?problemid=e446
 * Authored by Yang Yu-Chieh at 2023-05-18 15:56.
-* 從尾端開始看起，只要遇到數字就推進stack，
-* 遇到f,g,h就從stack中pop出需要的數量，
-* 最後再將函數運算結果丟回stack中。
+* 使用print會TLE
 """
 from sys import stdin,stdout
 from itertools import permutations
