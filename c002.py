d94980b138007d8601b5f84d05b14f065aa5c3ea
jane8366608 <jane8366608@gmail.com>
2023-05-18 15:43:44 +0800

    Update c002.py

--- c002.py
+++ c002.py
@@ -2,7 +2,14 @@
 * ZeroJudge c002. 10696 - f91
 * https://zerojudge.tw/ShowProblem?problemid=c002
 * Authored by Yang Yu-Chieh at 2023-05-18 15:17.
-* 
+* 按照它的遞迴規則可以發現，
+* 只要輸入的N大於100則只需要算一次就有答案，
+* 即N-10；
+* 而N小於101時不管N是多少，
+* 跑到最後都會進入101~111的數字，
+* 不論是這之中的哪一個數字，
+* 最後都會進入101→91，
+* 所以解這題只需O(1)。
 """
 from sys import stdin
 def f91(N):
