fe5c125502e314622bc12d98ce1c6c802e91be87
jane8366608 <jane8366608@gmail.com>
2023-05-29 16:39:48 +0800

    Create a017.py

---
+++ a017.py
@@ -0,0 +1,9 @@
+"""
+* ZeroJudge a017. 五則運算
+* https://zerojudge.tw/ShowProblem?problemid=a017
+* Authored by Yang Yu-Chieh at 2023-05-29 15:12.
+* 
+"""
+from sys import stdin
+for line in stdin:
+    print(eval(line.replace("/","//")))
